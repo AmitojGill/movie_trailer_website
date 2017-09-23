7cc23e20d472bdb2491b15137233dae5d6d4da86
Amitoj Gill <amitoj.s.gill@gmail.com>
2017-09-23 14:52:10 -0700

    Shebang line added

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python2.7
+
 import webbrowser
 import os
 import re
+
 
 # Styles and scripting for the page
 main_page_head = '''
@@ -9,7 +12,7 @@ main_page_head = '''
     <title>Fresh Tomatoes!</title>
 
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
